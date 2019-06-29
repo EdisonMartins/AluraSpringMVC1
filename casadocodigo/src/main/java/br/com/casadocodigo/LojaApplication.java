e14790cefa7ab93a4cb17f5c4372b6e6523f3d38
Edison Martins <Edison Martins@DESKTOP-UBC9E7L>
2019-06-29 20:17:17 -0300

    Corrigido erro no Pom

--- casadocodigo/src/main/java/br/com/casadocodigo/LojaApplication.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/LojaApplication.java
@@ -1,13 +1,13 @@
-package com.alura.casadocodigo;
+package br.com.casadocodigo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CasadocodigoApplication {
+public class LojaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CasadocodigoApplication.class, args);
+		SpringApplication.run(LojaApplication.class, args);
 	}
 
 }
